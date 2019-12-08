5899e02c3217bfd3e7bd6bca7bbf3ccd5cbecc19
Mithilesh Srihari <mithileshsrihari@gmail.com>
2019-12-08 20:35:43 +0000

    changed the map file

--- map/views.py
+++ map/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
-import pandas as pd
 from django.http import HttpResponse
-import folium
-
-def index(requests):
-    csv_df = pd.read_csv('file.csv')
-    lat_long = list(zip(csv_df.y, csv_df.x))
-    maps = folium.Map(location = (40.7829,-73.9654), tiles = 'OpenStreetMap', max_zoom = 18, zoom_control=True, zoom_start=15)
-
-    for cor in lat_long:
-    
-        folium.Marker(location = [cor[0],cor[1]], fill_color='#43d9de', radius=8 ).add_to( maps ) 
-    maps.save('map.html')
-    
-    return HttpResponse(maps.get_root().render())
-
-# Create your views here.
+from sightings.models import Sightings
+from django.template import loader
+def index(request):
+    sightings = Sightings.objects.all()
+    template = loader.get_template('map/map.html')
+    context = {'sightings':sightings}
+    return HttpResponse(template.render(context, request))
