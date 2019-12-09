e6dd9616a453b1a5074c4eaaced79e195faf6dbb
Mithilesh Srihari <mithileshsrihari@gmail.com>
2019-12-09 02:22:04 +0000

    stats command added

--- sightings/urls.py
+++ sightings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('',views.index, name = 'index'),
     re_path(r'(\w+-[A-Za-z]{2}-\d{4}-\d{2})/', views.edit, name='edit'),
     path('add/',views.add, name = 'add'),
+    path('stats/',views.stats, name = 'stats'),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -3,6 +3,15 @@ from .models import Sightings
 from django.http import HttpResponse
 from django.template import loader
 from .forms import Sightingsform
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.dates import DateFormatter 
+from matplotlib import pylab
+from pylab import *
+import PIL, PIL.Image 
+from io import BytesIO
 
 def index(request):
     Unique_Squirrel_list = Sightings.objects.all()
@@ -49,7 +58,37 @@ def edit(request, Unique_Squirrel_ID):
     return render(request, 'sightings/edit.html/', {'form': form})
 
 def stats(request):
-    pass
+    s = Sightings.objects.all().values()
+    df=pd.DataFrame(s)
+    df2=pd.read_csv('file.csv')
+    df2.groupby('primary_fur_color').count()['x']
+    explode=(0,0,0)
+    labels='Black','Cinnamon','Gray'
+    s=df2.groupby('primary_fur_color').count()['x'].sum()
+    x1=(df2.groupby('primary_fur_color').count()['x'][0])/s*100
+    x2=(df2.groupby('primary_fur_color').count()['x'][1])/s*100
+    x3=(df2.groupby('primary_fur_color').count()['x'][2])/s*100
+    sizes=[x1,x2,x3]
+    lst=[df2.groupby('shift').count()['x'][f] for f in range(2)]
+    x=['AM','PM']
+
+    fig, (ax1, ax2) = plt.subplots(2)
+    ax1.pie(sizes, explode=explode, labels=labels)
+    ax2.bar(x, height=lst)
+    ax2.title.set_text('Variations in shift sightings')
+    ax2.set_xlabel('Shift')
+    ax2.set_ylabel('Frequency of Sightings')
+
+    buffer = BytesIO()
+    canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    pilImage.save(buffer,"PNG")
+    pylab.close()
+
+    return HttpResponse(buffer.getvalue(), content_type= "image/png")
+
+
 
 
 
